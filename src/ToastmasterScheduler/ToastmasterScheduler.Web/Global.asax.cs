8d4d70ce26b26a7603baef5f8f5d361e1e8fa12a
zeng.stephen@gmail.com <zeng.stephen@gmail.com>
2014-11-02 15:22:42 +0800

    Setup RavenDB

--- src/ToastmasterScheduler/ToastmasterScheduler.Web/Global.asax.cs
+++ src/ToastmasterScheduler/ToastmasterScheduler.Web/Global.asax.cs
@@ -1,21 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Raven.Client;
+using Raven.Client.Document;
 
 namespace ToastmasterScheduler.Web
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        public static IDocumentStore DocumentStore { get; private set; }
+
+        public MvcApplication()
+        {
+            BeginRequest += (s, a) =>
+            {
+                HttpContext.Current.Items["CurrentRequestRevanSession"] = DocumentStore.OpenSession();
+            };
+
+            EndRequest += (s, a) =>
+            {
+                using (var session = (IDocumentSession) HttpContext.Current.Items["CurrentRequestRavenSession"])
+                {
+                    if (session == null) return;
+                    if (Server.GetLastError() != null) return;
+
+                    session.SaveChanges();
+                }
+            };
+        }
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            InitializeDocumentStore();
+        }
+
+        private static void InitializeDocumentStore()
+        {
+            if (DocumentStore != null) return;
+
+            DocumentStore = new DocumentStore()
+            {
+                ConnectionStringName = "RavenDB"
+            }.Initialize();
         }
     }
 }
--- src/ToastmasterScheduler/ToastmasterScheduler.Web/Startup.cs
+++ src/ToastmasterScheduler/ToastmasterScheduler.Web/Startup.cs
@@ -1,14 +1,14 @@
 ﻿using Microsoft.Owin;
 using Owin;
 
-[assembly: OwinStartupAttribute(typeof(ToastmasterScheduler.Web.Startup))]
+[assembly: OwinStartup(typeof(ToastmasterScheduler.Web.Startup))]
+
 namespace ToastmasterScheduler.Web
 {
-    public partial class Startup
+    public class Startup
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
         }
     }
 }
